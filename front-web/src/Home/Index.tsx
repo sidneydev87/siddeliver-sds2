70e5c693b79fa20d619d24aebe8407997e0811a7
unknown <sidney.pericia@outlook.com>
2021-01-07 13:21:37 -0300

    Add Navbar

---
+++ front-web/src/Home/Index.tsx
@@ -0,0 +1,19 @@
+import './styles.css';
+
+
+function Home() {
+    return (
+       <div className="home-container">
+         <div className="home-content"></div>
+         
+       </div> 
+
+
+
+    )
+
+
+}
+
+
+export default Home;
